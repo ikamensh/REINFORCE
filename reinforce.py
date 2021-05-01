19d6338fb14bb9f5aab3c6b519ce4786dc5ab783
ikkamens <ikkamens@amazon.com>
2021-05-01 12:43:29 +0200

    autodiff

--- reinforce.py
+++ reinforce.py
@@ -1,36 +1,29 @@
-import math
 import random
 
-from pennpaper import Metric, plot, plot_group
+import torch as t
+from pennpaper import Metric, plot_group
 
 from short_coridor import ShortCoridorEnv
 
-def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
-
 
 class CoridorAgent:
-
     def __init__(self):
-        self.param = 6 * ( random.random() - 0.5 )
+        self.param = t.tensor(6 * (random.random() - 0.5), requires_grad=True)
 
     def policy(self) -> int:
-        prob_1 = sigmoid(self.param)
-        if random.random() < prob_1:
+        if random.random() < self.prob(1):
             return 1
         else:
             return 0
 
-    def grad(self, action) -> float:
+    def prob(self, action) -> t.Tensor:
         assert action in (0, 1)
 
-        s = sigmoid(self.param)
-        sigm_grad = s * (1-s)
+        prob1 = t.sigmoid(self.param)
         if action == 0:
-            return -sigm_grad
+            return 1 - prob1
         else:
-            return sigm_grad
-
+            return prob1
 
 
 def reinforce(lr: float):
@@ -40,7 +33,7 @@ def reinforce(lr: float):
     env = ShortCoridorEnv()
     agent = CoridorAgent()
 
-    for episode in range(3_000):
+    for episode in range(1_000):
         history, total_reward = rollout(agent, env)
         m.add_record(episode, total_reward)
         G = sum(r for o, a, r in history)
@@ -56,7 +49,8 @@ def rollout(agent, env):
     history = []
     total_reward = 0
     while not done:
-        action = agent.policy()
+        with t.no_grad():
+            action = agent.policy()
         obs, rew, done, _ = env.step(action)
         total_reward += rew
         history.append((obs, action, rew))
@@ -64,17 +58,33 @@ def rollout(agent, env):
 
 
 def apply_grad(G, a, agent, lr):
-    prob_1 = sigmoid(agent.param)
-    prob = prob_1 if a == 1 else 1 - prob_1
-    update = lr * G * agent.grad(a) / ( prob + 1e-10)
-    agent.param += update
+    if agent.param.grad is not None:
+        agent.param.grad.zero_()
+    prob = agent.prob(a)
+    prob.backward()
+    with t.no_grad():
+        update = lr * G * agent.param.grad / (prob + 1e-10)
+        agent.param += update
 
 
-m1 = sum( reinforce(2 ** -13) for _ in range(100) )
-m2 = sum( reinforce(2 ** -12) for _ in range(100) )
-m3 = sum( reinforce(2 ** -14) for _ in range(100) )
-m4 = sum( reinforce(2 ** -15) for _ in range(100) )
-m5 = sum( reinforce(2 ** -11) for _ in range(100) )
-# m6 = sum( reinforce(2 ** -10) for _ in range(100) )
+import time
 
-plot_group([m1, m2, m3, m4, m5,], smoothen=True, stdev_factor=0.3, name="reinforce_coridor")
+start = time.time()
+
+random.seed(0)
+m1 = sum(reinforce(2 ** -13) for _ in range(10))
+random.seed(0)
+m2 = sum(reinforce(2 ** -12) for _ in range(10))
+random.seed(0)
+m3 = sum(reinforce(2 ** -14) for _ in range(10))
+
+plot_group(
+    [
+        m1,
+        m2,
+        m3,
+    ],
+    smoothen=True,
+    stdev_factor=0.3,
+    name=f"reinforce_coridor_{time.time() - start:.3f}",
+)
